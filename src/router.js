5f69308bfd6e8a968d029f4fdae4802bcd3b91be
FORTUNA <fortunaalya@gmail.com>
2020-11-06 13:19:40 +0700

    GD VueCLI

--- src/router.js
+++ src/router.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-
 function importComponent(path) {
     return () => import(`./components/${path}.vue`)
 }
 
 Vue.use(VueRouter);
-
 const router = new VueRouter({
     mode: "history",
     routes: [
@@ -17,20 +15,20 @@ const router = new VueRouter({
             name: "admin",
             component: importComponent('DashboardLayout'),
             children: [
-            //Dashboard
-            {
-            path: "/",
-            name: "Root",
-            component: importComponent('Dashboard'),
-            },
-            //To do list
-            {
-            path: "/gd",
-            name: "Guided",
-            component: importComponent('TodoList/List'),
-            },
-        ]
-    },
-]
+                //Dashboard
+                {
+                    path: "/",
+                    name: "Root",
+                    component: importComponent('Dashboard'),
+                },
+                // To do list
+                {
+                    path: "/gd",
+                    name: "Guided",
+                    component: importComponent('TodoList/List'),
+                },
+            ]
+        },
+    ]
 });
 export default router;
